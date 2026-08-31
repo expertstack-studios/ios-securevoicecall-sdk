51718ebbfc151260cdc5060890475a308f7f87d1
Azure DevOps <azure-pipelines@users.noreply.github.com>
2026-08-31 23:47:32 +0000

    Release SecuredCallsVoiceSDK 1.0.27-rc.2

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 		    name: "SecuredCallsVoiceSDKBinary",
-		    url: "https://github.com/expertstack-studios/ios-securevoicecall-sdk/releases/download/1.0.27-rc.1/SecuredCallsVoiceSDK.xcframework.zip",
-		    checksum: "b0b33c21bfe7f5db92af1e468bb1a7a6c1f4e31ca59c5f1e99a6827bd13e32bd"
+		    url: "https://github.com/expertstack-studios/ios-securevoicecall-sdk/releases/download/1.0.27-rc.2/SecuredCallsVoiceSDK.xcframework.zip",
+		    checksum: "2babdacc063fa57ea45d28eb57bc3f8723a65c6da0aba27db94dbf940cab1e61"
 		),
 		.target(
 			name: "SecuredCallsVoiceSDKWrapper",
